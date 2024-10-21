405a2e0a55235023d473b117df7e9e1b118aca93
cleomarlisboa <cleomar.srv@gmail.com>
2024-10-21 08:55:38 -0300

    correcao start.sh e Dockerfile

--- start.sh
+++ start.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
 
 # permissoes corretas para o nginx
-RUN chown -R www-data:www-data /var/www/storage
-RUN chmod -R 775 /var/www/storage
+chown -R www-data:www-data /var/www/storage
+chmod -R 775 /var/www/storage
+
+cd /var/www
+
+# Instalar o Composer
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('sha384', 'composer-setup.php') === 'dac665fdc30fdd8ec78b38b9800061b4150413ff2e3b6f88543c636f7cd84f6db9189d43a81e5503cda447da73c7e5b6') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+mv composer.phar /usr/local/bin/composer
+wait
 
 # Instalar dependências do PHP via Composer
-RUN composer install
-
-# Instalar dependências do Node.js e executar a build
-RUN npm install
+composer install --prefer-dist --no-interaction --optimize-autoloader
+wait
 
 # Executa as migrações do Laravel
-php artisan migrate
+php artisan migrate --force
 
 # povoar o banco de dados com usuarios, tweets, comentarios  e funcao Seguir
 php artisan db:seed
 
-# Inicia o npm em modo aberto a todos os hosts
-npm run dev -- --host &
-
 # Inicia o servidor PHP-FPM
 php-fpm
-
-
