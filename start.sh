ec35e32087281715217f85a5f6ebd5822cae35b0
cleomarlisboa <cleomar.srv@gmail.com>
2024-10-20 21:41:21 -0300

    configuracao docker - ajustes finais

--- start.sh
+++ start.sh
@@ -3,11 +3,13 @@
 # Executa as migrações do Laravel
 php artisan migrate
 
-# Instala o Breeze
-php artisan breeze:install blade
+# povoar o banco de dados com usuarios, tweets, comentarios  e funcao Seguir
+php artisan db:seed
 
 # Inicia o npm em modo aberto a todos os hosts
 npm run dev -- --host &
 
 # Inicia o servidor PHP-FPM
 php-fpm
+
+
